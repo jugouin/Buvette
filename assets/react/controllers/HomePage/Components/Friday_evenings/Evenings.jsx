67c772baf3ae4a84c79561024611fe387eb76f92
Julie Gouin <juliegouin@outlook.com>
2024-06-05 10:59:58 +0200

    admin

--- assets/react/controllers/HomePage/Components/Friday_evenings/Evenings.jsx
+++ assets/react/controllers/HomePage/Components/Friday_evenings/Evenings.jsx
@@ -16,7 +16,7 @@ export default function () {
                 <p>Venez déguster les menus du vendredi soir, puis laissez-vous emporter par les rythmes envoûtants d'un groupe de musique live surprise. Réservez dès maintenant pour ne pas manquer cette expérience unique !</p>
             </article>
             <div className="evening_button">
-                <a className="button"></a>
+                <a href="/chemin-symfony" className="button">Réserver</a>
             </div>
         </div> 
     </div>

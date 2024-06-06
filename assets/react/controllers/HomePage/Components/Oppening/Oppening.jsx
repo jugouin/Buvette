fbea7d2e936196b4b3b70931eef31febf989e689
Julie Gouin <juliegouin@outlook.com>
2024-06-06 21:48:11 +0200

    mobile design

--- assets/react/controllers/HomePage/Components/Oppening/Oppening.jsx
+++ assets/react/controllers/HomePage/Components/Oppening/Oppening.jsx
@@ -6,17 +6,17 @@ export default function () {
     return <div className="oppening_container">
         <img src={image} alt="Image" className='oppening_img' />
         <div className="oppening_text_section">
-            <div className="oppening_text_element">
+            <div className="oppening_text_element animated-oppening-text">
                 <h3 className="oppening_text_title">Ouverture</h3>
                 <p className="oppening_text">Tous les jours de 8h à 22h<br/>
                 Soirée spéciale tous les vendredis</p>
             </div>
-            <div className="oppening_text_element">
+            <div className="oppening_text_element animated-oppening-text">
                 <h3 className="oppening_text_title">Localisation</h3>
                 <p className="oppening_text">Route de la plage 3<br/>
                 1897 Port-Valais, Suisse</p>
             </div>
-            <div className="oppening_text_element">
+            <div className="oppening_text_element animated-oppening-text">
                 <h3 className="oppening_text_title">Contact</h3>
                 <p className="oppening_text">
                     Stéfanie Palmisano<br/>
